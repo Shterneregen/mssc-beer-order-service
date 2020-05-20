605a040053acca9641bdbfa38afb7f193005f670
shterneregen <shterneregen@gmail.com>
2020-05-21 01:54:46 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# MSSC Beer Order Service
+
+Spring Boot Microservice example
+
+---
+* Original repo:
+[MSSC Beer Order Service](https://github.com/springframeworkguru/mssc-beer-order-service)
+* Course: 
+[Spring Boot Microservices with Spring Cloud Beginner to Guru](https://www.udemy.com/course/spring-boot-microservices-with-spring-cloud-beginner-to-guru/)
---
+++ src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderMapper.java
@@ -0,0 +1,14 @@
+package guru.sfg.beer.order.service.web.mappers;
+
+import guru.sfg.beer.order.service.domain.BeerOrder;
+import guru.sfg.beer.order.service.web.model.BeerOrderDto;
+import org.mapstruct.Mapper;
+
+
+@Mapper(componentModel = "spring", uses = {DateMapper.class, BeerOrderLineMapper.class})
+public interface BeerOrderMapper {
+
+    BeerOrderDto beerOrderToDto(BeerOrder beerOrder);
+
+    BeerOrder dtoToBeerOrder(BeerOrderDto dto);
+}
