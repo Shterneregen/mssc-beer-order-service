83389549f2b1df73e54502a49d23cf43783fc919
shterneregen <shterneregen@gmail.com>
2020-06-24 23:31:24 +0300

    adding validation transitions

--- src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -6,8 +6,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 
 import java.util.EnumSet;
+
+import static guru.sfg.beer.order.service.domain.BeerOrderEventEnum.VALIDATE_ORDER;
+import static guru.sfg.beer.order.service.domain.BeerOrderEventEnum.VALIDATION_FAILED;
+import static guru.sfg.beer.order.service.domain.BeerOrderEventEnum.VALIDATION_PASSED;
+import static guru.sfg.beer.order.service.domain.BeerOrderStatusEnum.DELIVERED;
+import static guru.sfg.beer.order.service.domain.BeerOrderStatusEnum.DELIVERY_EXCEPTION;
+import static guru.sfg.beer.order.service.domain.BeerOrderStatusEnum.NEW;
+import static guru.sfg.beer.order.service.domain.BeerOrderStatusEnum.PICKED_UP;
+import static guru.sfg.beer.order.service.domain.BeerOrderStatusEnum.VALIDATED;
+import static guru.sfg.beer.order.service.domain.BeerOrderStatusEnum.VALIDATION_EXCEPTION;
 
 @Configuration
 @EnableStateMachineFactory
@@ -17,12 +28,20 @@ public class BeerOrderStateMachineConfig
     @Override
     public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> states) throws Exception {
         states.withStates()
-                .initial(BeerOrderStatusEnum.NEW)
+                .initial(NEW)
                 .states(EnumSet.allOf(BeerOrderStatusEnum.class))
-                .end(BeerOrderStatusEnum.DELIVERED)
-                .end(BeerOrderStatusEnum.PICKED_UP)
-                .end(BeerOrderStatusEnum.DELIVERY_EXCEPTION)
-                .end(BeerOrderStatusEnum.VALIDATED)
-                .end(BeerOrderStatusEnum.VALIDATION_EXCEPTION);
+                .end(DELIVERED)
+                .end(PICKED_UP)
+                .end(DELIVERY_EXCEPTION)
+                .end(VALIDATED)
+                .end(VALIDATION_EXCEPTION);
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> transitions) throws Exception {
+        transitions
+                .withExternal().source(NEW).target(NEW).event(VALIDATE_ORDER).and()
+                .withExternal().source(NEW).target(VALIDATED).event(VALIDATION_PASSED).and()
+                .withExternal().source(NEW).target(VALIDATION_EXCEPTION).event(VALIDATION_FAILED);
     }
 }
